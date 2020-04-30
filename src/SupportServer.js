2199f4c8f045e35f054a053f4372ed412fc8b6eb
Michael Flaherty <michaelwflaherty@me.com>
2020-04-30 12:13:51 -0700

    Log all support server errors

--- src/SupportServer.js
+++ src/SupportServer.js
@@ -27,19 +27,19 @@ export default class SupportServer {
      */
     async postVote(userid)
     {
-        if (!this.client.dbl_log)
-            return;
-
-        /**
-         * @type {Channel}
-         */
-        let channel = await this.client.channels.fetch(this.client.dbl_log);
-        if (!channel)
-            return;
-        
-        let user = await this.client.users.fetch(userid);
         try {
-            await channel.send(`${user.tag} has just voted for us on top.gg!  :heart:`)
+            if (!this.client.dbl_log)
+                return;
+
+            /**
+             * @type {Channel}
+             */
+            let channel = await this.client.channels.fetch(this.client.dbl_log);
+            if (!channel)
+                return;
+            
+            let user = await this.client.users.fetch(userid);
+                await channel.send(`${user.tag} has just voted for us on top.gg!  :heart:`)
         }
         catch (err) {
             log.error(`SupportServer#postVote -> ${err}`);
@@ -53,29 +53,29 @@ export default class SupportServer {
      */
     async postJoined(guild)
     {
-        if (!this.client.join_log)
-            return;
-
-        /**
-         * @type {Channel}
-         */
-        let channel = await this.client.channels.fetch(this.client.join_log);
-        if (!channel)
-            return;
-
-        const embed = new MessageEmbed()
-        .setThumbnail(guild.iconURL)
-        .setTitle('Server Joined:')    
-        .setColor(0x00FF00)
-        .addField("Name", guild.name, true)
-        .addField("Guild Id",  guild.id, true)
-        .addField("Total Members", guild.memberCount, true)
-        .addField("Total Channels", guild.channels.cache.size, true)
-        .addField("Guild Owner", guild.owner.user.tag, true)
-        .addField("Guild Region", guild.region, true)
-        .addField("Creation Date", guild.createdAt.toISOString(), true)
-
         try {
+            if (!this.client.join_log)
+                return;
+
+            /**
+             * @type {Channel}
+             */
+            let channel = await this.client.channels.fetch(this.client.join_log);
+            if (!channel)
+                return;
+
+            const embed = new MessageEmbed()
+            .setThumbnail(guild.iconURL)
+            .setTitle('Server Joined:')    
+            .setColor(0x00FF00)
+            .addField("Name", guild.name, true)
+            .addField("Guild Id",  guild.id, true)
+            .addField("Total Members", guild.memberCount, true)
+            .addField("Total Channels", guild.channels.cache.size, true)
+            .addField("Guild Owner", guild.owner.user.tag, true)
+            .addField("Guild Region", guild.region, true)
+            .addField("Creation Date", guild.createdAt.toISOString(), true)
+
             await channel.send(embed)
         }
         catch (err) {
@@ -90,29 +90,29 @@ export default class SupportServer {
      */
     async postLeft(guild)
     {
-        if (!this.client.join_log)
-            return;
-
-        /**
-         * @type {Channel}
-         */
-        let channel = await this.client.channels.fetch(this.client.join_log);
-        if (!channel)
-            return;
-
-        const embed = new MessageEmbed()
-        .setThumbnail(guild.iconURL)
-        .setTitle('Server Left:')    
-        .setColor(0xFF0000)
-        .addField("Name", guild.name, true)
-        .addField("Guild Id",  guild.id, true)
-        .addField("Total Members", guild.memberCount, true)
-        .addField("Total Channels", guild.channels.cache.array.length, true)
-        .addField("Guild Owner", guild.owner.user.tag, true)
-        .addField("Guild Region", guild.region, true)
-        .addField("Creation Date", guild.createdAt.toISOString(), true)
-
         try {
+            if (!this.client.join_log)
+                return;
+
+            /**
+             * @type {Channel}
+             */
+            let channel = await this.client.channels.fetch(this.client.join_log);
+            if (!channel)
+                return;
+
+            const embed = new MessageEmbed()
+            .setThumbnail(guild.iconURL)
+            .setTitle('Server Left:')    
+            .setColor(0xFF0000)
+            .addField("Name", guild.name, true)
+            .addField("Guild Id",  guild.id, true)
+            .addField("Total Members", guild.memberCount, true)
+            .addField("Total Channels", guild.channels.cache.array.length, true)
+            .addField("Guild Owner", guild.owner.user.tag, true)
+            .addField("Guild Region", guild.region, true)
+            .addField("Creation Date", guild.createdAt.toISOString(), true)
+
             await channel.send(embed)
         }
         catch (err) {
@@ -121,39 +121,40 @@ export default class SupportServer {
     }
 
     async postCompilation(code, lang, url, author, guild, success, failoutput) {
-        if (!this.client.compile_log)
-            return;
-
-        /**
-         * @type {Channel}
-         */
-        let channel = await this.client.channels.fetch(this.client.compile_log);
-        if (!channel)
-            return;
-
-        if (code.length >= 1017) {
-            code = code.substring(0, 1016);
-        }
-        if (failoutput) {
-            if (failoutput.length > 1017) {
-                failoutput = failoutput.substring(0, 1016);
-            }
-        }
-
-        const embed = new MessageEmbed()
-        .setTitle('Compilation Requested:')    
-        .setColor((success)?0x00FF00:0xFF0000)
-        .addField("Language", lang, true)
-        .addField("URL",  url, true)
-        .addField("User",  author.tag, true)
-        .addField("User ID",  author.id, true)
-        .addField("Guild",  guild.name, true)
-        .addField("Guild ID",  guild.id, true)
-        .addField('Code', `\`\`\`${code}\n\`\`\`\n`);
-        if (!success)
-            embed.addField('Compiler Output', `\`\`\`${failoutput}\n\`\`\`\n`);
-        
         try {
+
+            if (!this.client.compile_log)
+                return;
+
+            /**
+             * @type {Channel}
+             */
+            let channel = await this.client.channels.fetch(this.client.compile_log);
+            if (!channel)
+                return;
+
+            if (code.length >= 1017) {
+                code = code.substring(0, 1016);
+            }
+            if (failoutput) {
+                if (failoutput.length > 1017) {
+                    failoutput = failoutput.substring(0, 1016);
+                }
+            }
+
+            const embed = new MessageEmbed()
+            .setTitle('Compilation Requested:')    
+            .setColor((success)?0x00FF00:0xFF0000)
+            .addField("Language", lang, true)
+            .addField("URL",  url, true)
+            .addField("User",  author.tag, true)
+            .addField("User ID",  author.id, true)
+            .addField("Guild",  guild.name, true)
+            .addField("Guild ID",  guild.id, true)
+            .addField('Code', `\`\`\`${code}\n\`\`\`\n`);
+            if (!success)
+                embed.addField('Compiler Output', `\`\`\`${failoutput}\n\`\`\`\n`);
+            
             await channel.send(embed)
         }
         catch (err) {
