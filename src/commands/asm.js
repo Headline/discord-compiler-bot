eacbc69b173f6b89b71c551f2ddb983cf6626d28
Michael Flaherty <michaelwflaherty@me.com>
2020-05-11 21:11:27 -0700

    Improve asm compilers lookup failure response

--- src/commands/asm.js
+++ src/commands/asm.js
@@ -43,26 +43,29 @@ export default class AsmCommand extends CompilerCommand {
 
             const lang = args[0]
             const language = this.client.godbolt.findLanguageByAlias(lang)
-            if (language)
+            if (!language)
             {
-                let lookupName = language.name.toLowerCase();
-                let items = [];
-                this.client.godbolt.compilers.forEach((compiler) => {
-                    if (lookupName == compiler.lang.toLowerCase())
-                        items.push(`${compiler.name}: **${compiler.id}**`);
-                });
+                msg.replyFail(`'${lang}' is not a supported language for the ${this.toString()} command`);
+                return;
+            }
+            
+            let lookupName = language.name.toLowerCase();
+            let items = [];
+            this.client.godbolt.compilers.forEach((compiler) => {
+                if (lookupName == compiler.lang.toLowerCase())
+                    items.push(`${compiler.name}: **${compiler.id}**`);
+            });
 
-                let menu = new DiscordMessageMenu(msg.message, `Valid Godbolt '${lookupName}' compilers:`, 0x00FF00, 15, `Select a bold name on the right to use in place of the language in the ${this.client.prefix}asm command!`);
-                menu.buildMenu(items);
-                
-                try {
-                    await menu.displayPage(0);
-                    return;
-                }
-                catch (error) {
-                    msg.replyFail('Error with menu system, am I missing permissions?\n' + error);
-                    return;
-                }
+            let menu = new DiscordMessageMenu(msg.message, `Valid Godbolt '${lookupName}' compilers:`, 0x00FF00, 15, `Select a bold name on the right to use in place of the language in the ${this.client.prefix}asm command!`);
+            menu.buildMenu(items);
+            
+            try {
+                await menu.displayPage(0);
+                return;
+            }
+            catch (error) {
+                msg.replyFail('Error with menu system, am I missing permissions?\n' + error);
+                return;
             }
         }
 
