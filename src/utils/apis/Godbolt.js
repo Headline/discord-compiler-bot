f387eac466c7503304a1f36c513675358aed3b3f
Michael Flaherty <michaelwflaherty@me.com>
2020-05-12 18:18:25 -0700

    Resolve aliases in asm command

--- src/utils/apis/Godbolt.js
+++ src/utils/apis/Godbolt.js
@@ -113,7 +113,7 @@ export class Godbolt extends CompilationService {
             lang = lang.id;
         }
 
-        return this.find((l) => l.id === lang) != null;;
+        return this.findLanguageByAlias(lang) != null;;
     }
 
     /**
