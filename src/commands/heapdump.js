ab22d3c513be96c7216b7cebf42e1b35c84278a9
Michael Flaherty <michaelwflaherty@me.com>
2020-09-29 10:24:02 -0700

    Create heapdump command

---
+++ src/commands/heapdump.js
@@ -0,0 +1,45 @@
+import { MessageEmbed } from 'discord.js'
+
+import CompilerCommand from './utils/CompilerCommand'
+import CompilerCommandMessage from './utils/CompilerCommandMessage'
+import CompilerClient from '../CompilerClient'
+
+import heapdump from 'heapdump';
+
+export default class HeapdumpCommand extends CompilerCommand {
+    /**
+     *  Creates the blacklist command
+     * 
+     * @param {CompilerClient} client
+     */    
+    constructor(client) {
+        super(client, {
+            name: 'heapdump',
+            description: 'Dumps the bot\'s heap for inspection',
+            developerOnly: true,
+        });
+    }
+
+    /**
+     * Function which is executed when the command is requested by a user
+     *
+     * @param {CompilerCommandMessage} msg
+     */
+    async run(msg) {
+        
+        heapdump.writeSnapshot((err, string) => {
+            if (err) {
+                msg.dispatch(err.message);
+                return;
+            }
+            
+            const embed = new MessageEmbed()
+            .setTitle('Chrome Heapdump')
+            .setDescription(`Written to file:\n \`${string}\``)
+            .setThumbnail('https://imgur.com/PVBdOYi.png')
+            .setColor(0x046604)
+            .setFooter(`Requested by: ${msg.message.author.tag}`)
+            msg.dispatch('', embed);
+        });
+    }
+}
