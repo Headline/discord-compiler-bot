019b67c3be0a78921e758e02c8f452e05f61c9f8
Michael Flaherty <michaelwflaherty@me.com>
2020-10-19 12:05:25 -0700

    Remove now-unused privledged intent

--- src/main.rs
+++ src/main.rs
@@ -83,7 +83,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .framework(framework)
         .event_handler(events::Handler)
         .add_intent(GatewayIntents::GUILDS)
-        .add_intent(GatewayIntents::GUILD_MEMBERS)
         .add_intent(GatewayIntents::GUILD_MESSAGES)
         .add_intent(GatewayIntents::GUILD_MESSAGE_REACTIONS)
         .await?;
