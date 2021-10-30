d7399bc4b176de4bebadcab3401f9ff98dd9ba1f
Michael Flaherty <michaelwflaherty@me.com>
2021-10-30 07:08:37 -0700

    Fix position of test annotation for tests module

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,1 +1,3 @@
+#[cfg(test)]
+
 pub mod parser;
--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -1,5 +1,3 @@
-#[cfg(test)]
-
 use serenity::model::user::User;
 use crate::utls::parser::get_components;
 use crate::managers::compilation::CompilationManager;
