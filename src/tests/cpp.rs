ea2309ed289128bdcb661fd479bdbd6f9c0faeba
Michael Flaherty <michaelwflaherty@me.com>
2021-11-11 00:37:33 -0800

    Fix CppEval import

--- src/tests/cpp.rs
+++ src/tests/cpp.rs
@@ -1,55 +1,55 @@
-use crate::cppeval::eval;
+use crate::cppeval::eval::CppEval;
 
 #[tokio::test]
 async fn eval_output() {
-    let mut eval = eval::CppEval::new("<< \"test\"");
+    let mut eval = CppEval::new("<< \"test\"");
     assert!(eval.evaluate().is_ok());
 }
 
 #[tokio::test]
 async fn eval_output_implicit_main() {
-    let mut eval = eval::CppEval::new("{cout << \"test\"}");
+    let mut eval = CppEval::new("{cout << \"test\"}");
     assert!(eval.evaluate().is_ok());
 }
 
 #[tokio::test]
 async fn eval_output_explicit_main() {
-    let mut eval = eval::CppEval::new("int main(void)\n{cout << \"test\";}");
+    let mut eval = CppEval::new("int main(void)\n{cout << \"test\";}");
     assert!(eval.evaluate().is_ok());
 }
 
 #[tokio::test]
 async fn eval_output_balance_brace() {
-    let mut eval = eval::CppEval::new("{cout << \"{{{{{\";}");
+    let mut eval = CppEval::new("{cout << \"{{{{{\";}");
     assert!(eval.evaluate().is_ok());
 }
 
 #[tokio::test]
 async fn eval_output_balance_brace2() {
-    let mut eval = eval::CppEval::new("{cout << \"}}}}}\";}");
+    let mut eval = CppEval::new("{cout << \"}}}}}\";}");
     assert!(eval.evaluate().is_ok());
 }
 
 #[tokio::test]
 async fn eval_output_balance_brace_fail() {
-    let mut eval = eval::CppEval::new("{ {cout << \"}}}}}\";}");
+    let mut eval = CppEval::new("{ {cout << \"}}}}}\";}");
     assert!(eval.evaluate().is_err()); // expecting error
 }
 
 #[tokio::test]
 async fn eval_output_custom_func() {
-    let mut eval = eval::CppEval::new("<< f(2); int f(int a) { return a * 4; }");
+    let mut eval = CppEval::new("<< f(2); int f(int a) { return a * 4; }");
     assert!(eval.evaluate().is_ok());
 }
 
 #[tokio::test]
 async fn eval_output_discord_escape() {
-    let mut eval = eval::CppEval::new("`<< f(2); int f(int a) { return a * 4; }`");
+    let mut eval = CppEval::new("`<< f(2); int f(int a) { return a * 4; }`");
     assert!(eval.evaluate().is_ok());
 }
 
 #[tokio::test]
 async fn eval_output_conditional() {
-    let mut eval = eval::CppEval::new("{ int a = 4; if (a > 3) { cout << \"true\"; } }");
+    let mut eval = CppEval::new("{ int a = 4; if (a > 3) { cout << \"true\"; } }");
     assert!(eval.evaluate().is_ok());
 }
