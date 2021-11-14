cdd51e5287289f4c720fa40d3c0b74fe5a62db4d
Michael Flaherty <michaelwflaherty@me.com>
2021-11-13 21:04:45 -0800

    Fix errors on one line parsing

--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -237,3 +237,48 @@ async fn standard_parse_late_deduce_compiler_block_stdin() {
     assert_eq!(parser_result.url, "");
     assert_eq!(parser_result.code, "console.log(\"beehee\");\n");
 }
+
+#[tokio::test]
+async fn standard_parse_one_line() {
+    let dummy_user = User::default();
+    let input = indoc::indoc!(
+        ";compile js ```console.log(\"beehee\");```"
+    );
+
+    let reply = None;
+    let cm = Arc::new(RwLock::new(CompilationManager::new().await.unwrap()));
+    let result = get_components(input, &dummy_user, Some(&cm), &reply).await;
+    if let Err(_) = &result {
+        assert!(false, "Parser failed.");
+    }
+
+    let parser_result = result.unwrap();
+    assert_eq!(parser_result.target, "javascript");
+    assert_eq!(parser_result.args.len(), 0);
+    assert_eq!(parser_result.options.len(), 0);
+    assert_eq!(parser_result.stdin, "");
+    assert_eq!(parser_result.url, "");
+    assert_eq!(parser_result.code, "console.log(\"beehee\");");
+}
+
+#[tokio::test]
+async fn standard_parse_args_one_line() {
+    let dummy_user = User::default();
+    let input = indoc::indoc!(
+        ";compile c -O3```int main() {return 232;}```"
+    );
+
+    let reply = None;
+    let result = get_components(input, &dummy_user, None, &reply).await;
+    if let Err(_) = &result {
+        assert!(false, "Parser failed.");
+    }
+
+    let parser_result = result.unwrap();
+    assert_eq!(parser_result.target, "c");
+    assert_eq!(parser_result.args.len(), 0);
+    assert_eq!(parser_result.options, ["-O3"]);
+    assert_eq!(parser_result.stdin, "");
+    assert_eq!(parser_result.url, "");
+    assert_eq!(parser_result.code, "int main() {return 232;}");
+}
