9420b1d12d1bea1862dc195ae37e04a2039942a1
Headline <michaelwflaherty@me.com>
2022-04-30 01:23:32 -0700

    Format guild counts in 'k' number format for instances with larger than 10k guilds (#153)

--- src/utls/discordhelpers/mod.rs
+++ src/utls/discordhelpers/mod.rs
@@ -210,8 +210,16 @@ pub async fn manual_dispatch(http: Arc<Http>, id: u64, emb: CreateEmbed) {
 }
 
 pub async fn send_global_presence(shard_manager : &MutexGuard<'_, ShardManager>, sum : u64) {
+    let server_count = {
+        if sum < 10000 {
+            sum.to_string()
+        }
+        else {
+            format!("{:.1}k", sum/1000)
+        }
+    };
     // update shard guild count & presence
-    let presence_str = format!("in {} servers | ;invite", sum);
+    let presence_str = format!("in {} servers | ;invite", server_count);
 
     let runners = shard_manager.runners.lock().await;
     for (_, v) in runners.iter() {
